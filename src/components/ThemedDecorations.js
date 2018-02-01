5c2b89540d00b7a4af373ded20dc848604af6638
Brandon Villa <bornofos@gmail.com>
2018-02-01 14:22:57 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,15 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+export default class Invitation extends React.Component {
+
+  render() {
+    const childrenProps = React.Children.map(this.props.children, child => {
+      return (
+        <div className="">{child}</div>
+      )
+    })
+    return(
+      {childrenProps}
+    )
+  }
+}
