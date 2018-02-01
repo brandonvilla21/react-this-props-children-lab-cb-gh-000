46d69a135569b374dd00078469755c987fcaebdf
Brandon Villa <bornofos@gmail.com>
2018-02-01 14:19:14 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,9 @@
 // Code Invitation Component Here
+import React from 'react';
+export default class Invitation extends React.Component {
+  render() {
+    return (
+      <h1>You have been invited!</h1>
+    )
+  }
+}
